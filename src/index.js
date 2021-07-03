9b908699689550d8a652331a885b04be52682c6d
andy8421 <yichang8421@qq.com>
2021-07-03 15:04:16 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import x from './x.js';
 
 console.log(x);
+console.log('hinihao');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js',
+        filename: 'index.[contenthash].js',
         // path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '我de页面',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                // 查找以.css结尾的文件
+                use: ["style-loader", "css-loader"],
+                // css-loader将css文件读入js文件
+            //     style-loader将读到的css样式放入header中
+            },
+        ],
     },
 };
