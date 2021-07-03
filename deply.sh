b3232386b7e3e64941595502069551423a2eee4c
andy8421 <yichang8421@qq.com>
2021-07-03 20:13:50 +0800

    add deploy.sh

---
+++ deply.sh
@@ -0,0 +1,10 @@
+yarn build &&
+git checkout gh-pages &&
+rm -rf src *.sh *.js *.json yarn.lock;
+rm -rf *.html *.js *.css *.png &&
+mv dist/* ./;
+rm -rf dist;
+git add . &&
+git commit -m 'autoUpdate' &&
+git push; &&
+git checkout master;
