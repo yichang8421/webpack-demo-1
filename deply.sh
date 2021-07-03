829a9bcbace9066d8b1e53f0dd9393b5225fb564
andy8421 <yichang8421@qq.com>
2021-07-03 22:25:12 +0800

    添加说明

--- deply.sh
+++ deply.sh
@@ -3,6 +3,12 @@ git checkout gh-pages &&
 rm -rf *.html *.js *.css *.png &&
 mv dist/* ./ &&
 rm -rf dist;
+
+# 以下命令执行报错
+# error: open("deply.sh"): Permission denied 
+# error: unable to index file 'deply.sh'
+# fatal: adding files failed
+# 原因不明
 git add . &&
 git commit -m 'update' &&
 git push && 
