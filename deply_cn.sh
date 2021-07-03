42cc4f20ee8128a9c164a6f401ea10e53283a8be
andy8421 <yichang8421@qq.com>
2021-07-03 22:42:38 +0800

    add gitee

---
+++ deply_cn.sh
@@ -0,0 +1,15 @@
+yarn build &&
+git checkout gh-pages &&
+rm -rf *.html *.js *.css *.png &&
+mv dist/* ./ &&
+rm -rf dist;
+
+# 以下命令执行报错
+# error: open("deply.sh"): Permission denied 
+# error: unable to index file 'deply.sh'
+# fatal: adding files failed
+# 原因不明
+git add . &&
+git commit -m 'update' &&
+git push gitee gh-pages:master && 
+git checkout -
