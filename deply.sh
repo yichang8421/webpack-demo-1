ab0b7c6f840ecb30bf522759619c482f73eb941a
andy8421 <yichang8421@qq.com>
2021-07-03 21:51:15 +0800

    updated

--- deply.sh
+++ deply.sh
@@ -1,11 +1,5 @@
 yarn build &&
 git checkout gh-pages &&
-#rm -rf src *.js *.json yarn.lock;
 rm -rf *.html *.js *.css *.png &&
 mv dist/* ./ &&
 rm -rf dist;
-
-git add . &&
-git commit -m 'autoUpdate' &&
-#git push &&
-git checkout -;
